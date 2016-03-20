310d215fadca995834f0ea181b0c97ad3798134d
PanVicePrezes <panviceprezes@gmail.com>
2016-03-21 00:14:40 +0100

    Final app structure ???2

---
+++ __latest/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class LatestConfig(AppConfig):
+    name = '__latest'
---
+++ __rankings/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+	template = loader.get_template('__rankings/public/index.html')
+	context = {
+		'imie': 'Artur',
+		'nazwisko': 'Slomowski',
+		'dataUrodzenia': '1990',
+	}
+	return HttpResponse(template.render(context, request))
